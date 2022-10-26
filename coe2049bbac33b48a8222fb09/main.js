1671ef46992cc7492d7c25110df3fb1d6e8df200
jv-emia016 <116307042+jv-emia016@users.noreply.github.com>
2022-10-27 06:28:42 +0800

    Add files via upload

---
+++ coe2049bbac33b48a8222fb09/main.js
@@ -0,0 +1,136 @@
+const btnPlay = document.querySelector("#btn-play");
+const btnPlayIcon = document.querySelector("#btn-play-icon");
+const btnRepeat = document.querySelector("#btn-repeat");
+const btnPrev = document.querySelector("#btn-prev");
+const btnNext = document.querySelector("#btn-next");
+const btnVolume = document.querySelector("#btn-volume");
+const btnVolumeIcon = document.querySelector("#btn-volume i");
+const playerVolume = document.querySelector("#player-volume");
+const songName = document.querySelector("#song-name");
+const songAuthor = document.querySelector("#song-author");
+const playerCurrentTime = document.querySelector("#player-current-time");
+const playerDuration = document.querySelector("#player-duration");
+const playerProgress = document.querySelector("#player-progress");
+const audioPlayer = document.querySelector("#audio-player");
+
+let currentSong = 0;
+let repeatSong = false;
+
+const songs = [
+  {
+    name: "Island of Paradise",
+    author: "Tim TaJ",
+    path: "assets/TimTaj_-_Island_of_Paradise.mp3",
+  },
+  
+   {
+    name: "Summer Inspiring Tropical",
+    author: "Korandrino",
+    path: "assets/Korandrino_-_Summer_Inspiring_Tropical.mp3",
+  },
+  
+  {
+    name: "The Tropical House",
+    author: "Tim Taj",
+    path: "assets/TimTaj_-_The_Tropical_House.mp3",
+  },
+  
+    {
+    name: "Inspiring Tropical Pop No Vox",
+    author: "Sound For You",
+    path: "assets/SoundForYou_-_Inspiring_Tropical_Pop_No_Vox.mp3",
+  },
+  
+  {
+    name: "Tropical",
+    author: "Tim Taj",
+    path: "assets/TimTaj_-_Tropical.mp3",
+  },
+  
+ 
+];
+
+btnPlay.addEventListener("click", () => togglePlaySong());
+btnPrev.addEventListener("click", () => changeSong(false));
+btnNext.addEventListener("click", () => changeSong());
+btnRepeat.addEventListener("click", () => toggleRepeatSong());
+playerVolume.addEventListener("input", () => changeVolume());
+playerProgress.addEventListener("input", () => changeTime());
+audioPlayer.addEventListener("timeupdate", () => timeUpdate());
+audioPlayer.addEventListener("ended", () => ended());
+
+const togglePlaySong = () => {
+  if (audioPlayer.paused) {
+    audioPlayer.play();
+    btnPlayIcon.classList.replace("bi-play-fill", "bi-pause-fill");
+  } else {
+    audioPlayer.pause();
+    btnPlayIcon.classList.replace("bi-pause-fill", "bi-play-fill");
+  }
+};
+
+const changeSong = (next = true) => {
+  if (next && currentSong < songs.length - 1) {
+    currentSong++;
+  } else if (!next && currentSong > 0) {
+    currentSong--;
+  } else {
+    return;
+  }
+
+  updatePlayer();
+  togglePlaySong();
+};
+
+const updatePlayer = () => {
+  const song = songs[currentSong];
+
+  songName.innerHTML = song.name;
+  songAuthor.innerHTML = song.author;
+  audioPlayer.src = song.path;
+  playerProgress.value = audioPlayer.currentTime;
+};
+
+const toggleRepeatSong = () => {
+  repeatSong = !repeatSong;
+  btnRepeat.classList.toggle("btn-activated");
+};
+
+const timeUpdate = () => {
+  const { currentTime, duration } = audioPlayer;
+
+  if (isNaN(duration)) return;
+
+  playerDuration.innerHTML = formatSecondsToMinutes(duration);
+  playerCurrentTime.innerHTML = formatSecondsToMinutes(currentTime);
+  playerProgress.max = duration;
+  playerProgress.value = currentTime;
+};
+
+const changeVolume = () => {
+  const { value } = playerVolume;
+
+  audioPlayer.volume = value;
+
+  if (value == 0) {
+    btnVolumeIcon.classList.replace("bi-volume-up-fill", "bi-volume-mute-fill");
+  } else {
+    btnVolumeIcon.classList.replace("bi-volume-mute-fill", "bi-volume-up-fill");
+  }
+};
+
+const changeTime = () => {
+  audioPlayer.currentTime = playerProgress.value;
+};
+
+const formatSecondsToMinutes = (seconds) => {
+  return new Date(seconds * 1000).toISOString().slice(14, 19);
+};
+
+const ended = () => {
+  repeatSong ? togglePlaySong() : changeSong(true);
+};
+
+window.onload = () => {
+  updatePlayer();
+};
